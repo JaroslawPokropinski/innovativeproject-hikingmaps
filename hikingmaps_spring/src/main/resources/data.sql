18cc6377b3b8d773e094c5f84b0ea72e6dcc223d
Jarosław Pokropiński <jarek.pokropinski@gmail.com>
2021-09-05 21:49:19 +0200

    Dockerize app

--- hikingmaps_spring/src/main/resources/data.sql
+++ hikingmaps_spring/src/main/resources/data.sql
@@ -1,5 +1,21 @@
-insert into user
-values(1,TRUE, 'admin@email.com', 'admin', '$2a$10$qE8OR/FNG7XXfKmCJIjGfuEQEZdBkWCJ2kb0WvJwibg5q2DR8Z2ry');
+insert into
+  user
+values
+  (
+    1,
+    TRUE,
+    'admin@email.com',
+    'admin',
+    '$2a$04$.Y/aPpdRPVXUeE6RSyALUuHwtDmYAqE3yhSeEHzwUsv60TPyZ9LVa'
+  );
 
-insert into user
-values(2,FALSE, 'user@email.com', 'user', '$2a$10$qE8OR/FNG7XXfKmCJIjGfuEQEZdBkWCJ2kb0WvJwibg5q2DR8Z2ry');
+insert into
+  user
+values
+  (
+    2,
+    FALSE,
+    'user@email.com',
+    'user',
+    '$2a$04$.Y/aPpdRPVXUeE6RSyALUuHwtDmYAqE3yhSeEHzwUsv60TPyZ9LVa'
+  );
